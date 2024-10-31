8b08d225717cb65457da027051c134a695ab88ed
GOWTHAM735628 <zxso735601@gmail.com>
2024-11-01 03:51:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyrogram import Cilent, filters
 
 API_ID = "24619371"
 API_HASH = "4418d775c222fa4f61c4c50e56bb5fef"
-BOT_TOKEN = "7902715438:AAGXKhRx5W15amS8Pctc_QNAnkLl1qY9ptE"
+BOT_TOKEN = "7600157728:AAFoVEVXlOs4IPxj104i_Aj-OHoNM2el_EE"
 
 Tamilnetworkcom = Cilent(
       name="codebot
