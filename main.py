d4883d135a11833dbcce7d13a5c39687c356cf0e
GOWTHAM735628 <zxso735601@gmail.com>
2024-11-01 03:40:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,21 @@ from pyrogram import Cilent, filters
 
 
 API_ID = "24619371"
-API_HASH = ""
-BOT_TOKEN = ""
+API_HASH = "4418d775c222fa4f61c4c50e56bb5fef"
+BOT_TOKEN = "7902715438:AAGXKhRx5W15amS8Pctc_QNAnkLl1qY9ptE"
+
+Tamilnetworkcom = Cilent(
+      name="codebot
+      api_id=API_ID,
+      api_hash=API_HASH,
+      bot_token=BOT_TOKEN,
+) 
+
+@tamilnetworkcom.on_message(filters.command("start"))
+async def start_com(cilent, message):
+  await message.reply_text("Hi 👋,
+This is An Advanced and yet powerful Bot")
+
+print("Bot was Started")
+
+Tamilnetworkcom.run()                           
